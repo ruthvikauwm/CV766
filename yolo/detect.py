afe67aaa85eb544ceb60d6ab0421cc57412bed2d
Ruthvika Reddy Loka <ruthvikareddy@cherry.cs.wisc.edu>
2023-04-28 15:33:27 -0500

    Save annotations and confidence of test images

--- yolo/detect.py
+++ yolo/detect.py
@@ -80,6 +80,8 @@ def run(
         dnn=False,  # use OpenCV DNN for ONNX inference
         vid_stride=1,  # video frame-rate stride
 ):
+    save_txt=True
+    save_conf=True
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
